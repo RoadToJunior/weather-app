adc13590c3fd491b108fd8e763a91dc11cdbeb15
JSisLove <kontodarm@wp.pl>
2023-11-08 13:55:32 +0100

    add styles to main container

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 function App() {
   return (
-<>
+<div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-600 to-blue-700 h-fit shadow-xl shadow-gray-400'>
   <Search/>
-</>
+</div>
   );
 }
 
