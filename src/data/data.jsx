3b3fbddd17875c64095c40695572e178bbab8865
JSisLove <kontodarm@wp.pl>
2023-11-08 14:01:07 +0100

    :tada: init

---
+++ src/data/data.jsx
@@ -0,0 +1,22 @@
+export const cities = [
+    {
+        id: 1,
+        city: "London",
+    },
+    {
+        id: 2,
+        city: "London",
+    },
+    {
+        id: 3,
+        city: "London",
+    },
+    {
+        id: 4,
+        city: "London",
+    },
+    {
+        id: 5,
+        city: "London",
+    }
+]
