2825a98bb8e1d54a55a3316a77d8c75355640f80
JSisLove <kontodarm@wp.pl>
2023-11-11 13:27:58 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Search from './components/Search';
 import React from 'react';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
 <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-600 to-blue-700 h-fit shadow-xl shadow-gray-400'>
-  <Search/>
+  <TopButtons/>
+  <Inputs/>
 </div>
   );
 }
